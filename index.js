8be0e5d10d508c93df0f4c2ac3d69020724f7b3d
SrijaC2 <128138613+SrijaC2@users.noreply.github.com>
2024-04-11 16:22:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ const app = require("./app");
 app.listen(3000, () => {
   console.log("Started express server at port 3000");
 });
-
-console.log("From branch 2");
-console.log("From main branch");
-
