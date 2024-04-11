d91f06f984db0e871fe42b2d571e5b6d067a0e27
SrijaC2 <srijachanda06@gmail.com>
2024-04-11 17:24:07 +0530

    hello

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ const app = require("./app");
 app.listen(3000, () => {
   console.log("Started express server at port 3000");
 });
-console.log("I am from branch 2");
-console.log("I am from main branch");
-
